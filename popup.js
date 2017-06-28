e84f81aa10fb52b19d75c27304ea0b38524312f7
Khushboo Agrawal <web.khushboo@gmail.com>
2017-06-29 01:09:54 +0530

    Check in starting files for budget manager

---
+++ popup.js
@@ -0,0 +1,23 @@
+
+$(function(){
+     chrome.storage.sync.get(['total','limit'],function(budget){
+ $("#total").text(budget.total );
+ $("#limit").text(budget.limit);
+     });
+    $('#spendAmount').click(function(){
+       chrome.storage.sync.get('total',function(budget){
+           var newTotal=0;
+            if(budget.total){
+                newTotal += parseInt(budget.total);
+            }
+
+            var amount = $("#amount").val();
+            if(amount){
+                newTotal += parseInt(amount); 
+            }
+            chrome.storage.sync.set({'total':newTotal})
+            $("#total").text(newTotal);
+            $("#amount").val('');
+       }); 
+    });
+}); 
