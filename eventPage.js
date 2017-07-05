4a8175863b138a3a03f1dee7f835febdfa21417d
Khushboo Agrawal <web.khushboo@gmail.com>
2017-07-05 23:29:37 +0530

    Added badges in budget manager extension

--- eventPage.js
+++ eventPage.js
@@ -37,3 +37,8 @@
        }
    }
   });
+
+
+  chrome.storage.onChanged.addListener(function(changes,storageName){
+     chrome.browserAction.setBadgeText({"text":changes.total.newValue.toString()});
+  });
